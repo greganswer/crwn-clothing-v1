a92565735d98c2c54e35b5621e11b7c278a280ca
Greg Answer <greganswer@gmail.com>
2019-09-12 00:01:14 -0400

    Handle signup and sign in using Firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,9 +14,37 @@ const config = {
 
 firebase.initializeApp(config);
 
+// First or create an authenticated user.
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  // Create the user if it doesn't exist.
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+// Setup Google sign in.
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
