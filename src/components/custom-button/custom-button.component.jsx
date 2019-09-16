b14c5af0383aafec221655463361f7c5cdc0ed37
Greg Answer <greganswer@gmail.com>
2019-09-16 11:33:22 -0400

    Persist cart state in local storage

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({
-  children, inverted, isGoogleSignIn, ...otherProps
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
 }) => (
-  <button
-      className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    <button
+      className={`${inverted ? 'inverted' : ''} ${
+        isGoogleSignIn ? 'google-sign-in' : ''
+        } custom-button`}
       {...otherProps}
     >
       {children}
     </button>
-);
+  );
 
 export default CustomButton;
